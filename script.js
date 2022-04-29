c8cf82c53c71146a9a34b36e2088df697a90a9ca
HarshPrateek559 <neutronspace@gmail.com>
2022-04-29 22:20:03 +0530

    completed the mobile layout of the project

---
+++ script.js
@@ -0,0 +1,15 @@
+let daily = document.getElementById("daily");
+let weekly = document.getElementById("weekly");
+let monthly = document.getElementById("monthly");
+let mydata = JSON.parse(data)
+daily.onclick = ()=>{
+    console.log(mydata)
+}
+
+weekly.onclick = ()=>{
+
+}
+
+monthly.onclick = ()=>{
+
+}
